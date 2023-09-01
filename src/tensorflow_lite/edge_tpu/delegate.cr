46bbcff9979c84079c539a48c1fed77c1823aa47
Stephen von Takach <steve@place.technology>
2023-09-01 11:24:22 +1000

    feat(edge_tpu/delegate): expose dev path and type

--- src/tensorflow_lite/edge_tpu/delegate.cr
+++ src/tensorflow_lite/edge_tpu/delegate.cr
@@ -2,11 +2,17 @@ require "../delegate"
 
 # a delegate that can be used with InterpreterOptions#add_delegate
 class TensorflowLite::EdgeTPU::Delegate < TensorflowLite::Delegate
-  def initialize(type, path : String)
+  def initialize(type : EdgeTPU::Device::Type, path : String)
     options_ptr = Pointer(LibEdgeTPU::Option).null
     delegate_ptr = LibEdgeTPU.create_delegate(type, path, options_ptr, 0)
     @to_unsafe = delegate_ptr
+
+    @path = path
+    @type = type
   end
+
+  @path : String
+  @type : EdgeTPU::Device::Type
 
   # :nodoc:
   def finalize
